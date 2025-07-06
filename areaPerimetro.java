65032d4483bc69368ec346d6f4ee9ad0d1cf8abc
Renata Claudino <saraiva.claudino@gmail.com>
2025-07-06 18:28:08 -0300

    Exercício Perimetro

---
+++ areaPerimetro.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class areaPerimetro {
+    public static void main(String [] args) {
+        float altura, largura, area, perimetro;
+        Scanner entrada;
+        entrada = new Scanner (System.in);
+        System.out.print("Digite o valor da altura do retângulo: ");
+        altura = entrada.nextFloat();
+        System.out.print("Digite o valor da largura do retângulo: ");
+        largura = entrada.nextFloat();
+        area = calculaArea (altura, largura);
+        perimetro = calculaPerimetro(altura, largura);
+        mostraMensagem("O valor da área é ", area);
+        mostraMensagem("O valor do perímetro é ", perimetro);
+    }
+    public static float calculaArea (float a, float b) {
+        return a * b;
+    }
+    public static float calculaPerimetro (float a, float b){
+        return 2* a + 2*b;
+    }
+    public static void mostraMensagem (String msg, float vlr) {
+        System.out.println (msg+vlr);
+    }
+}
