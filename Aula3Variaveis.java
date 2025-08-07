c172496df50a708d5d70af959c8878d90ed73344
Renata Claudino <saraiva.claudino@gmail.com>
2025-08-07 18:18:25 -0300

    Aula 3

---
+++ Aula3Variaveis.java
@@ -0,0 +1,14 @@
+public class Aula3Variaveis {
+    public static void main(String[] args) {
+        String nome = "Renata";
+        int idade = 32;
+        double peso = 104.5;
+        boolean gostadeJava = true;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Peso: " + peso);
+        System.out.println("Gosta de Java: " + gostadeJava);
+
+    }
+}
